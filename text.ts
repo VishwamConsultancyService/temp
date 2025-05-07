1f20651d684195789762a867ae7d78850ccab182
VishwamConsultancyService <118435599+VishwamConsultancyService@users.noreply.github.com>
2025-05-07 10:31:50 +1000

    Update text.ts

--- text.ts
+++ text.ts
@@ -1,19 +1,26 @@
-<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-<AssignMessage async="false" continueOnError="false" enabled="true" name="AM-CaptureTargetRequestDetails">
-    <DisplayName>AM-CaptureTargetRequestDetails</DisplayName>
-    <AssignVariable>
-        <Name>private.api.targetRequestContent</Name>
-        <Ref>request.content</Ref>
-    </AssignVariable>
-    <IgnoreUnresolvedVariables>true</IgnoreUnresolvedVariables>
-    <AssignTo createNew="false" transport="http" type="request"/>
-</AssignMessage>
+default
+{
+	"logLevel": "ALL",
+	"dataMask": {
+		"mask": "sample",
+		"maskConfig": "sample Config"
+	}
+}
 
-	----
-
-	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-<SharedFlow name="default">
-    <Step>
-        <Name>AM-CaptureTargetRequestDetails</Name>
-    </Step>
-</SharedFlow>
+{
+	"logLevel": "ALL",
+	"dataMask": {
+		"mask": "true",
+		"maskConfig": [{
+				"tagToMask": "username",
+				"maskChar": "*",
+				"keepLastChars": 0
+			},
+			{
+				"tagToMask": "password",
+				"maskChar": "*",
+				"keepLastChars": 0
+			}
+		]
+	}
+}
