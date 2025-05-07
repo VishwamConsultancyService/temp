eeadfd786272ada6413fa298f307184e2d08f1b4
VishwamConsultancyService <118435599+VishwamConsultancyService@users.noreply.github.com>
2025-05-07 11:03:16 +1000

    Update text.ts

--- text.ts
+++ text.ts
@@ -1,26 +1,17 @@
-default
-{
-	"logLevel": "ALL",
-	"dataMask": {
-		"mask": "sample",
-		"maskConfig": "sample Config"
-	}
-}
-
-{
-	"logLevel": "ALL",
-	"dataMask": {
-		"mask": "true",
-		"maskConfig": [{
-				"tagToMask": "username",
-				"maskChar": "*",
-				"keepLastChars": 0
-			},
-			{
-				"tagToMask": "password",
-				"maskChar": "*",
-				"keepLastChars": 0
-			}
-		]
-	}
-}
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<MessageLogging async="false" continueOnError="false" enabled="true" name="ML-CommonLogAPIMetadata">
+    <DisplayName>ML-CommonLogAPIMetadata</DisplayName>
+    <Syslog>
+        <Message>{api.logMessage}</Message>
+        <Host>internal.sunsuper.com.au</Host>
+        <Port>8086</Port>
+        <Protocol>TCP</Protocol>
+        <SSLInfo>
+            <Enabled>true</Enabled>
+            <ClientAuthEnabled>true</ClientAuthEnabled>
+            <KeyStore>ref://splunkKeystoreEdgeSunSuperClientKeyRef</KeyStore>
+            <KeyAlias>splunk-key</KeyAlias>
+            <!--<TrustStore>ref://chTrustRefTest</TrustStore>-->
+        </SSLInfo>
+    </Syslog>
+</MessageLogging>
