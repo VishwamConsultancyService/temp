2d89f380e4c515f06885a9146106a6be3e67616a
VishwamConsultancyService <118435599+VishwamConsultancyService@users.noreply.github.com>
2025-05-07 10:13:40 +1000

    Update text.ts

--- text.ts
+++ text.ts
@@ -1,20 +1,52 @@
 <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-<SharedFlow name="default">
-    <Step>
-        <Name>AM-CaptureRequestDetails</Name>
-    </Step>
-    <Step>
-        <Name>KVM-FetchApiLogConfig</Name>
-    </Step>
-    <Step>
-        <Name>KVM-FetchDefaultLogConfig</Name>
-        <Condition>(api.sf.kvm.logConfig equals "") or (api.sf.kvm.logConfig equals null)</Condition>
-    </Step>
-    <Step>
-        <Name>EV-SetLogVariables</Name>
-    </Step>
-</SharedFlow>
+<ExtractVariables async="false" continueOnError="false" enabled="true" name="EV-SetLogVariables">
+    <DisplayName>EV-SetLogVariables</DisplayName>
+    <Properties/>
+    <IgnoreUnresolvedVariables>true</IgnoreUnresolvedVariables>
+    <JSONPayload>
+        <Variable name="api.logLevel">
+            <JSONPath>$.logLevel</JSONPath>
+        </Variable>
+        <Variable name="api.maskLog">
+            <JSONPath>$.dataMask.mask</JSONPath>
+        </Variable>
+        <Variable name="api.maskConfig">
+            <JSONPath>$.dataMask.maskConfig</JSONPath>
+        </Variable>
+    </JSONPayload>
+    <Source clearPayload="false">api.sf.kvm.logConfig</Source>
+</ExtractVariables>
+
+-----
+
+    <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<KeyValueMapOperations async="false" continueOnError="false" enabled="true" name="KVM-FetchApiLogConfig" mapIdentifier="LoggingConfig">
+    <DisplayName>KVM-FetchApiLogConfig</DisplayName>
+    <Properties/>
+    <ExclusiveCache>false</ExclusiveCache>
+    <ExpiryTimeInSecs>5</ExpiryTimeInSecs>
+    <Get assignTo="api.sf.kvm.logConfig">
+        <Key>
+            <Parameter ref="apiproxy.name"/>
+        </Key>
+    </Get>
+    <Scope>environment</Scope>
+</KeyValueMapOperations>
+
 
 ----
 
-    
+
+    <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<KeyValueMapOperations async="false" continueOnError="false" enabled="true" name="KVM-FetchDefaultLogConfig" mapIdentifier="LoggingConfig">
+    <DisplayName>KVM-FetchDefaultLogConfig</DisplayName>
+    <Properties/>
+    <ExclusiveCache>false</ExclusiveCache>
+    <ExpiryTimeInSecs>5</ExpiryTimeInSecs>
+    <Get assignTo="api.sf.kvm.logConfig">
+        <Key>
+            <Parameter>default</Parameter>
+        </Key>
+    </Get>
+    <Scope>environment</Scope>
+</KeyValueMapOperations>
