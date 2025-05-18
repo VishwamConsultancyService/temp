9a812d199f44471a3f717f001bfe2a090cccd1d5
VishwamConsultancyService <118435599+VishwamConsultancyService@users.noreply.github.com>
2025-05-19 09:00:52 +1000

    Update text.ts

--- text.ts
+++ text.ts
@@ -1,17 +1,32 @@
-<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-<MessageLogging async="false" continueOnError="false" enabled="true" name="ML-CommonLogAPIMetadata">
-    <DisplayName>ML-CommonLogAPIMetadata</DisplayName>
-    <Syslog>
-        <Message>{api.logMessage}</Message>
-        <Host>internal.sunsuper.com.au</Host>
-        <Port>8086</Port>
-        <Protocol>TCP</Protocol>
-        <SSLInfo>
-            <Enabled>true</Enabled>
-            <ClientAuthEnabled>true</ClientAuthEnabled>
-            <KeyStore>ref://splunkKeystoreEdgeSunSuperClientKeyRef</KeyStore>
-            <KeyAlias>splunk-key</KeyAlias>
-            <!--<TrustStore>ref://chTrustRefTest</TrustStore>-->
-        </SSLInfo>
-    </Syslog>
-</MessageLogging>
+export const ResponseMessages = {
+  SUCCESS: {
+    code: "200",
+    reason: "Success",
+    message: "Operation completed successfully"
+  },
+  MATS_ENUM_FAILED: {
+    code: "500",
+    reason: "Partial Failure",
+    message: "One or more operations failed. Check MATS/ENUM results."
+  },
+  AUTH_FAILED: {
+    code: "401",
+    reason: "Authentication Failed",
+    message: "Unable to authenticate with backend"
+  },
+  INVALID_REQUEST: {
+    code: "400",
+    reason: "Bad Request",
+    message: "Request payload or parameters are invalid"
+  },
+  NOT_FOUND: {
+    code: "404",
+    reason: "Not Found",
+    message: "Resource not found"
+  },
+  INTERNAL_ERROR: {
+    code: "500",
+    reason: "Internal Server Error",
+    message: "Something went wrong on the server"
+  },
+};
